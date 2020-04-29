be5bba38d516762a0266086938b03e198485cab2
ManuelVassallo <56382051+ManuelVassallo@users.noreply.github.com>
2020-04-29 13:07:02 +0200

    Toolbar navigation setup

--- mcast-prospectus-2020-2021/src/app/prospectus/prospectus-header/prospectus-header.component.ts
+++ mcast-prospectus-2020-2021/src/app/prospectus/prospectus-header/prospectus-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prospectus-header',
@@ -9,7 +10,21 @@ export class ProspectusHeaderComponent implements OnInit {
 
   @Input("title") title: string ="";
 
-  constructor() { }
+  constructor(public router: Router) { }
+
+  //tool bar home button setup
+
+  home(){
+    this.router.navigate(['menu']);
+  }
+
+  //backlink setup
+
+  @Input('backlink') backlink: any = ['/'];
+
+  public navigate() {
+    this.router.navigateByUrl(this.backlink, { replaceUrl: true });
+  }
 
   ngOnInit() {}
 
