0e3c1bf81a5fe9011466b06db58e26bdc8189cc2
ManuelVassallo <56382051+ManuelVassallo@users.noreply.github.com>
2020-05-05 01:36:29 +0200

    Course Level Content update

--- mcast-prospectus-2020-2021/src/app/level/foundation-ica/foundation-ica.page.ts
+++ mcast-prospectus-2020-2021/src/app/level/foundation-ica/foundation-ica.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-foundation-ica',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoundationIcaPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
+
+  cn1(){
+    this.router.navigate(['']);
+  }
+
+  cn2(){
+    this.router.navigate(['']);
+  }
+
+  cn3(){
+    this.router.navigate(['']);
+  }
+
+  cn4(){
+    this.router.navigate(['']);
+  }
+
+  cn5(){
+    this.router.navigate(['']);
+  }
+
+  cn6(){
+    this.router.navigate(['']);
+  }
 
   ngOnInit() {
   }
