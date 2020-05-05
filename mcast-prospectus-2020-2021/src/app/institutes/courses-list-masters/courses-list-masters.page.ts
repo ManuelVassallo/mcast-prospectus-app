be24bf45b33f3c2532a736f52b1856739b4f7f61
ManuelVassallo <56382051+ManuelVassallo@users.noreply.github.com>
2020-05-05 18:14:49 +0200

    Button Setup for masters course list page

--- mcast-prospectus-2020-2021/src/app/institutes/courses-list-masters/courses-list-masters.page.ts
+++ mcast-prospectus-2020-2021/src/app/institutes/courses-list-masters/courses-list-masters.page.ts
@@ -11,23 +11,23 @@ export class CoursesListMastersPage implements OnInit {
   constructor(public router: Router) { }
 
   cn1(){
-    
+    this.router.navigate(['']);
   }
 
   cn2(){
-
+    this.router.navigate(['']);
   }
 
   cn3(){
-
+    this.router.navigate(['']);
   }
 
   cn4(){
-
+    this.router.navigate(['']);
   }
 
   cn5(){
-
+    this.router.navigate(['']);
   }
 
   ngOnInit() {
